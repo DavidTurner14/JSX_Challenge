59ae7d3a102c90c234acae9a782ad5886d5c2716
DavidTurner14 <51490155+DavidTurner14@users.noreply.github.com>
2021-02-07 10:53:02 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+//Create a react app from scratch.
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <ul>
+      <li>Dave</li>
+      <li>Asia</li>
+      <li>Donna</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
